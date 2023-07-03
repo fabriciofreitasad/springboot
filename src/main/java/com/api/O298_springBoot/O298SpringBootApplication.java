bd888b5197fbca2b4afceed9f5a0a5f235e79a5d
fabricio freitas oliveira <fabricio.freitas.ad@gmail.com>
2023-07-03 19:25:27 -0300

    Inicio do projeto com spring Boot

---
+++ src/main/java/com/api/O298_springBoot/O298SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.api.O298_springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O298SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O298SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/O298_springBoot/O298SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.O298_springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O298SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
